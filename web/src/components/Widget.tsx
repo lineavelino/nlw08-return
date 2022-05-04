5e38995515bdeac39b7a1dd8952efbc14cc93d88
Aline Avelino <aline.savelino@gmail.com>
2022-05-03 21:37:20 -0300

    expandir texto de feedback ao usar o tab

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -9,7 +9,7 @@ export function Widget() {
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6' />
 
-                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs group-focus-visible:max-w-xs transition-all duration-500 ease-linear'>
                     <span className='pl-2'></span>
                     Feedback
                 </span>
